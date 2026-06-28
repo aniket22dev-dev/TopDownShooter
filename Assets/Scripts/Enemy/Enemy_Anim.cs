815207b4fbaddb9c1732a6049723dcac462713b4
Ani <rishavsen6261@gmail.com>
2026-06-29 00:33:23 +0530

    Enemy Health Included

---
+++ Assets/Scripts/Enemy/Enemy_Anim.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+public class Enemy_Anim : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    private GameObject  Player;
+
+    private void Start()
+    {
+        Player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(this.transform.position, Player.transform.position);
+        
+        //Debug.Log($"Distance : {distance}");
+        if (distance < 9f)
+        {
+            transform.LookAt(Player.transform.position);
+            animator.SetBool("Shoot", true);
+        }
+    }
+}
--- Assets/Scripts/Enemy/OnDamage.cs
+++ Assets/Scripts/Enemy/OnDamage.cs
@@ -1,51 +1,118 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OnDamage : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    [SerializeField] private float health = 100;
-    AutoAimShooter AutoAimShooter;
+    [Header("Health")]
+    [SerializeField] private float maxHealth = 100f;
+    private float health;
+
+    [Header("UI")]
+    [SerializeField] private Slider healthSlider;
+
+    private AutoAimShooter autoAimShooter;
     private Animator anim;
-    // Update is called once per frame
+    private Rigidbody[] ragdollBodies;
+    private bool isDead = false;
+
+    private Coroutine hideHealthBarRoutine;
 
     private void Start()
     {
-        AutoAimShooter = FindFirstObjectByType<AutoAimShooter>();
-        anim = GetComponent<Animator>();
+        health = maxHealth;
 
+        autoAimShooter = FindFirstObjectByType<AutoAimShooter>();
+        anim = GetComponent<Animator>();
+        ragdollBodies = GetComponentsInChildren<Rigidbody>();
+
+        foreach (Rigidbody rb in ragdollBodies)
+        {
+            if (rb.gameObject == gameObject)
+                continue;
+
+            rb.isKinematic = true;
+        }
+
+        // Health bar setup
+        if (healthSlider != null)
+        {
+            healthSlider.maxValue = maxHealth;
+            healthSlider.value = maxHealth;
+            healthSlider.gameObject.SetActive(false);
+        }
     }
+
     public void TakeDamage(float damage)
     {
+        if (isDead)
+            return;
+
+        health -= damage;
+        health = Mathf.Clamp(health, 0, maxHealth);
+
+        // Show & update health bar
+        if (healthSlider != null)
+        {
+            healthSlider.gameObject.SetActive(true);
+            healthSlider.value = health;
+
+            if (hideHealthBarRoutine != null)
+                StopCoroutine(hideHealthBarRoutine);
+
+            hideHealthBarRoutine = StartCoroutine(HideHealthBar());
+        }
+
         if (health > 0)
         {
-            health -= damage;
             StartCoroutine(HitImpact());
-            Debug.Log($"Enemy took the damage : {damage}  current health {health}");
+            Debug.Log($"Enemy took {damage} damage. Remaining Health: {health}");
         }
-        if (health == 0 || health < 0)
+        else
         {
-            StartCoroutine(OnEnemyDead());
+            isDead = true;
+            OnEnemyDead();
+        }
+    }
+
+    private void OnEnemyDead()
+    {
+        if (healthSlider != null)
+            healthSlider.gameObject.SetActive(false);
+
+        CapsuleCollider collider = GetComponent<CapsuleCollider>();
+        if (collider != null)
+            collider.enabled = false;
+
+        anim.enabled = false;
+
+        foreach (Rigidbody rb in ragdollBodies)
+        {
+            if (rb.gameObject == gameObject)
+                continue;
+
+            rb.isKinematic = false;
         }
 
+        autoAimShooter.UnlockMovement();
 
+        Destroy(gameObject, 3f);
+
+        Debug.Log("Enemy Died");
     }
 
-    IEnumerator OnEnemyDead()
-    {
-        BoxCollider collider = GetComponent<BoxCollider>();
-        collider.enabled = false; // ? To Avoide triggring the lock movement 
-        anim.enabled=false;
-        yield return new WaitForSeconds(1f);
-        Destroy(gameObject,0.5f);
-        AutoAimShooter.UnlockMovement();
-        Debug.Log($"Health : {health} Enemy Died ");
-    }
     IEnumerator HitImpact()
     {
         anim.SetBool("Hit", true);
         yield return new WaitForSeconds(0.1f);
         anim.SetBool("Hit", false);
+    }
 
+    IEnumerator HideHealthBar()
+    {
+        yield return new WaitForSeconds(2f);
+
+        if (!isDead && healthSlider != null)
+            healthSlider.gameObject.SetActive(false);
     }
 }
